e7e13e928fa834290131f8da19920dc10da7f7ad
Brian <riceb53@gmail.com>
2018-07-24 18:45:07 -0500

    use figaro gem to hide API key

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,7 +1,7 @@
 class Api::NewsController < ApplicationController
   def index
     # get the data
-    response = Unirest.get("https://newsapi.org/v2/everything?q=#{params[:search_term]}&sortBy=publishedAt&apiKey=")
+    response = Unirest.get("https://newsapi.org/v2/everything?q=#{params[:search_term]}&sortBy=publishedAt&apiKey=#{ENV['NEWS_API_KEY']}")
     @info = response.body
     render 'index.json.jbuilder'
   end
